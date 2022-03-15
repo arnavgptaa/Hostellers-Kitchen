65031434b506f2d10bd558c0e1ce890810201d34
arnavgptaa <arnavgpta@gmail.com>
2022-03-15 11:49:19 +0530

    initial changes

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React      from 'react'
+import Restaurant from './components/Basics/Restaurant'
+// import UseState   from './components/Hooks/useState';
+// import UseEffect  from './components/Hooks/UseEffect';
+// import useReducer from './components/Hooks/useReducer';
+const App = () => {
+  return <Restaurant />
+}
+
+export default App;
